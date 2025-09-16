25dd781352e989f7b0f0fce983a699da1f0387b5
VerdoodtDries <dries@nordend.eu>
2025-09-16 10:41:30 +0200

    Add main for Windows

---
+++ main_windows_arango.py
@@ -0,0 +1,18 @@
+import logging
+from pathlib import Path
+
+from API.APIEnums import Environment, AuthType
+from DBPipelineController import DBPipelineController
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    settings_path = Path.home() / 'OneDrive - Nordend/projects/AWV/resources/settings_SyncToArangoDB.json'
+
+    # 🚀 Connect to ArangoDB using DBPipelineController
+    controller = DBPipelineController(settings_path=settings_path, auth_type=AuthType.JWT, env=Environment.PRD)
+    controller.run()
