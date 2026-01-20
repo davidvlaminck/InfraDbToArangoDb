0b2893558b015dd9f1f551574227733fe08573a9
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-01-20 10:58:15 +0100

    increased timeout

--- ArangoDBConnectionFactory.py
+++ ArangoDBConnectionFactory.py
@@ -3,7 +3,7 @@ from arango import ArangoClient
 
 class ArangoDBConnectionFactory:
     def __init__(self, db_name, username, password):
-        self.client = ArangoClient()
+        self.client = ArangoClient(request_timeout=600)
         self.db_name = db_name
         self.username = username
         self.password = password
