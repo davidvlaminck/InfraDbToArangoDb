35d0dd3cdbec1a43c38557772c46c0745a780fae
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-08-06 20:23:38 +0200

    merged

--- ArangoDBConnectionFactory.py
+++ ArangoDBConnectionFactory.py
@@ -1,16 +1,11 @@
-from typing import Optional
-
 from arango import ArangoClient
 
 
 class ArangoDBConnectionFactory:
-    def __init__(self, db_name: str, username: str, password: str, hosts: Optional[list[str]] = None):
+    def __init__(self, db_name, username, password):
         # Increase request timeout to allow long-running server operations (index builds, large AQL)
         # Default was 360s; bump to 1200s to avoid ReadTimeout during heavy operations.
-        if hosts:
-            self.client = ArangoClient(hosts=hosts, request_timeout=1200)
-        else:
-            self.client = ArangoClient(request_timeout=1200)
+        self.client = ArangoClient(request_timeout=1200)
         self.db_name = db_name
         self.username = username
         self.password = password
