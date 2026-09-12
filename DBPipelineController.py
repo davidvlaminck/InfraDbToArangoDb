e7c37a4de8bd8cf6c22acdda03d048de86836164
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-09-12 21:08:05 +0200

    hosts

--- DBPipelineController.py
+++ DBPipelineController.py
@@ -49,7 +49,8 @@ class DBPipelineController:
         db_name = db_settings['database']
         username = db_settings['user']
         password = db_settings['password']
-        factory = ArangoDBConnectionFactory(db_name, username, password)
+        hosts = db_settings.get('hosts')
+        factory = ArangoDBConnectionFactory(db_name, username, password, hosts=hosts)
 
         return factory, eminfra_client, emson_client
 
